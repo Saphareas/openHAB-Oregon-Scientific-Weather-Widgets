7f650e4a6729c45965836a70e10e3afdbefa47cc
Fabian Große <saphareas@googlemail.com>
2019-03-08 15:14:39 +0100

    Compact Widget

---
+++ README.md
@@ -0,0 +1,1 @@
+You need to copy the file ```habpanel_oscwidget_maximize.js``` to your ```<openhab>/conf/html_ folder```
---
+++ habpanel_oscwidget_maximize.js
@@ -0,0 +1,4 @@
+console.debug("Maximizing ");
+let parent = document.querySelector("main.osc-body").parentElement;
+//console.debug(parent);
+parent.classList.add("osc-maximize");
