83a7502e76a4bc0aa8489993a52f2f8917d37d55
Fabian Große <saphareas@googlemail.com>
2019-03-09 10:23:14 +0100

    Fix widget layout with ng Controller

--- habpanel_oscwidget_maximize.js
+++ habpanel_oscwidget_maximize.js
@@ -1,4 +1,27 @@
-console.debug("Maximizing ");
-let parent = document.querySelector("main.osc-body").parentElement;
-//console.debug(parent);
-parent.classList.add("osc-maximize");
+//Thanks to BasvanH (https://github.com/BasvanH) for this code
+
+(function () {
+    'use strict';
+    angular
+      .module('app.widgets.oscwidget', [])
+      .controller('ngMaximizeWidgetContent', ['$rootScope', '$scope',
+        function ($rootScope, $scope) {
+  
+          function maximize() {
+            console.debug("Maximizing ");
+            let parent = document.querySelector("main.osc-body");
+            if (parent != null) {
+                parent = parent.parentElement.parentElement;
+                parent.classList.add("osc-maximize");
+            }
+          }
+  
+          // Run function when items update
+          $rootScope.$on('openhab-update', function (event, item) {
+            maximize();
+          });
+  
+          maximize();
+        }
+      ]);
+  })();
